bc33e67c03bd1ad33970de91eeaadb2848f9fb18
suksien <hahasuksien@yahoo.com.my>
2024-09-09 21:42:55 +0800

    adding more features

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ before do
   @storage = DatabasePersistence.new(logger)
 end
 
-## GET routes
+### GET routes
 get "/" do
   @parks = @storage.get_all_parks
   erb :homepage
@@ -52,4 +52,38 @@ get "/parks-by-area" do
   @parks = @storage.get_all_parks
   @criteria = "area"
   erb :sort_parks
+end
+
+get "/:name/" do
+  @park = @storage.get_park(params[:name])
+  erb :park
+end
+
+def valid_date?(date)
+  return false if date == ""
+  year, month, date = date.split("-")
+  return false if year.nil? || month.nil? || date.nil?
+
+  if year.scan(/\D/).empty? & month.scan(/\D/).empty? & date.scan(/\D/).empty?
+    year.to_i.between?(1000, 9999) & month.to_i.between?(1, 12) & date.to_i.between?(1, 31)
+  else
+    false
+  end
+end
+
+# TBC (the POST path for update)
+get "/:name/update" do
+  @park = @storage.get_park(params[:name])
+  @update = true
+  erb :park
+end
+
+### POST routes
+post "/:name/edit" do
+  if valid_date?(params[:date_visited])
+    @storage.update_park_visit(params[:name], params[:date_visited], params[:note])
+  else
+    session[:error] = "Not a valid date. Please enter a date following the YYYY-MM-DD format. "
+  end
+  redirect "/#{params[:name]}/"
 end
--- database_persistence.rb
+++ database_persistence.rb
@@ -1,4 +1,5 @@
 require "pg"
+require "pry"
 
 class DatabasePersistence
   def initialize(logger)
@@ -21,6 +22,32 @@ class DatabasePersistence
 
     result = query(statement)
     result.map { |tuple| sql_out_to_hsh(tuple) }
+  end
+
+  def get_park(name)
+    statement = <<~sql
+      SELECT *
+      FROM park_info as p
+      JOIN visits as v
+      ON p.id = v.park_id
+      WHERE p.name = $1
+    sql
+
+    result = query(statement, name)
+    sql_out_to_hsh(result[0])
+  end
+
+  def update_park_visit(park_name, date_visited, note)
+    statement= "SELECT id FROM park_info WHERE name = $1"
+    park_id = query(statement, park_name)[0]["id"].to_i
+
+    statement = <<~sql
+      UPDATE visits
+      SET visited = true, date_visited = $1, note = $2
+      WHERE park_id = $3
+    sql
+
+    query(statement, date_visited, note, park_id)
   end
 
   private
